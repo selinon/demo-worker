2108fc4284d00dc19ac427924e2ae686d8348ba0
Fridolin Pokorny <fridolin@redhat.com>
2018-04-30 17:47:01 +0200

    Introduce dedicated dispatcher worker

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+import logging
 
 from celery.bin.celery import main as celery_main
+from selinon import Config
 
-# Act like we would invoke celery directly.
+from demo_worker import get_config_files
+
+_LOGGER = logging.getLogger(__name__)
+
+Config.set_config_yaml(*get_config_files())
+
+SELINON_DISPATCHER = bool(int(os.getenv('SELINON_DISPATCHER', '0')))
+INCLUDE_QUEUES = Config.dispatcher_queues if SELINON_DISPATCHER else Config.task_queues
+
+_LOGGER.info("Worker will listen on %r", INCLUDE_QUEUES)
+
+# Act like we would invoke celery directly from command line.
 sys.argv = [
     '/usr/bin/celery',
     'worker',
     '--app=entrypoint',
     '-l', 'INFO',
     '--concurrency=1',
-    '-Qfoo',
+    '-Q', ','.join(INCLUDE_QUEUES),
     '-P', 'solo',
     '--prefetch-multiplier=128',
     '-Ofair',
