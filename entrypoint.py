f5ff69021c07ef321fd1fc947cb53893b4f20e9f
Fridolin Pokorny <fridolin@redhat.com>
2018-04-30 14:39:30 +0200

    Accept configuration from env variables

--- entrypoint.py
+++ entrypoint.py
@@ -9,29 +9,34 @@ import os
 from celery import Celery
 from selinon import Config
 
+from demo_app import get_config_files
+
+
+BROKER_URL = os.environ['BROKER_URL']
+
 
 def init(with_result_backend=False):
-    """ Init Celery and Selinon
+    """Init Celery and Selinon.
 
     :param with_result_backend: true if the application should connect to the result backend
     :return: Celery application instance
     """
     conf = {
-        # TODO: get this from env variables
-        'broker_url': os.environ.get('BROKER_URL', 'amqp://rabbitmq:5672'),
+        'broker_url': BROKER_URL
     }
 
     if with_result_backend:
-        conf['result_backend'] = os.environ.get('RESULT_BACKEND_URL', 'redis://redis:6379/0')
+        conf['result_backend'] = os.environ['RESULT_BACKEND_URL']
 
     app = Celery('app')
     app.config_from_object(conf)
 
-
-    # Set Selinon configuration
-    Config.set_config_yaml(os.path.join(_BASE_NAME, 'nodes.yaml'), flow_definition_files)
+    # Set Selinon configuration.
+    Config.set_config_yaml(*get_config_files())
     # Prepare Celery
     Config.set_celery_app(app)
+
+    return app
 
 
 # Entrypoint for Celery worker
