ed13b5e6e01eb0b9675b3595db06e2430dbf4994
Fridolin Pokorny <fridolin@redhat.com>
2018-05-01 19:51:18 +0200

    Use organization and repo from repo argument

--- demo_worker/storages/travis_logs.py
+++ demo_worker/storages/travis_logs.py
@@ -6,11 +6,8 @@ from selinon.storages.s3 import S3
 class TravisLogsStorage(S3):
     def store(self, node_args, flow_name, task_name, task_id, result):
         # TODO: make this more generic and move generic parts to Selinon's S3 directly
-        object_key = 'travis/logs/{org}/{repo}/{build}'.format(
-            org=node_args['organization'],
-            repo=node_args['repo'],
-            build=node_args['build']
-        )
+        org, repo = node_args['repo'].split('/', maxsplit=1)
+        object_key = 'travis/logs/{org}/{repo}/{build}.json'.format(org=org, repo=repo, build=node_args['build'])
         result = json.dumps(result).encode()
         self._s3.Object(self._bucket_name, object_key).put(Body=result)
         return object_key
