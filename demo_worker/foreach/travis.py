07eb0d8e1c5f3c95c8ee47708c0f7049eab3c6a9
Fridolin Pokorny <fridolin@redhat.com>
2018-05-02 15:07:39 +0200

    Rename tasks

--- demo_worker/foreach/travis.py
+++ demo_worker/foreach/travis.py
@@ -8,7 +8,7 @@ _LOGGER = logging.getLogger(__name__)
 def iter_travis_repos(storage_pool: StoragePool, node_args: dict) -> list:
     """Iterate over repos and extend node args with the repo name."""
     try:
-        repos = storage_pool.get('travis_active_repos_task')
+        repos = storage_pool.get('TravisActiveRepos')
 
         new_node_args = []
         for repo in repos:
@@ -23,7 +23,7 @@ def iter_travis_repos(storage_pool: StoragePool, node_args: dict) -> list:
 def iter_travis_builds(storage_pool: StoragePool, node_args: dict) -> list:
     """Iterate over builds found and extend node args with the build information."""
     try:
-        builds = storage_pool.get('travis_repo_builds_task')
+        builds = storage_pool.get('TravisRepoBuilds')
 
         new_node_args = []
         for build in builds:
@@ -38,7 +38,7 @@ def iter_travis_builds(storage_pool: StoragePool, node_args: dict) -> list:
 def iter_travis_builds_count(storage_pool: StoragePool, node_args: dict) -> list:
     """Iterate over build counts respecting offset so build id downloads can be done in parallel."""
     try:
-        builds_count = storage_pool.get('travis_repo_builds_count_task')['count']
+        builds_count = storage_pool.get('TravisRepoBuildsCount')['count']
 
         new_node_args = []
         for offset in range(builds_count):
