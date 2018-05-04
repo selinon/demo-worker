5a91736d74ed237a34cd38cfe099982867054c10
Fridolin Pokorny <fridolin@redhat.com>
2018-05-04 09:11:49 +0200

    Cleanup logs from special characters
    * Introduces a new task that cleans logs from special characters
    * Migration is done for the running flows causing tainted flows to retry

--- demo_worker/storages/travis_logs.py
+++ demo_worker/storages/travis_logs.py
@@ -5,7 +5,7 @@ from selinon.storages.s3 import S3
 
 class TravisLogsStorage(S3):
     def store(self, node_args, flow_name, task_name, task_id, result):
-        object_key = 'travis/logs/{org}/{repo}/1/{build}.json'.format(
+        object_key = 'travis/logs/{org}/{repo}/2/{build}.json'.format(
             org=node_args['organization'],
             repo=node_args['repo'],
             build=node_args['build']
